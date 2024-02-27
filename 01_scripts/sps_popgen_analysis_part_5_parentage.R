0dd0486c55f2c19ea69fa132f3b6687ee9ea079e
bensutherland <bensutherland7@gmail.com>
2024-02-27 15:27:33 -0800

    Add F0 and F1 analysis to end of script

--- 01_scripts/sps_popgen_analysis_part_5_parentage.R
+++ 01_scripts/sps_popgen_analysis_part_5_parentage.R
@@ -52,4 +52,81 @@ graph_relatives(input.FN = "03_results/ckmr_input_rubias_142_ind_347_loc_2024-02
 )
 
 
+#### Add the F1 vs F0 ####
+## Pilot, first generation, all loci
+input_rubias.FN <- "03_results/rubias_142_ind_380_loc_2024-02-27.txt" # Pilot
+parent_pop <- "VIU_F0"
+offspring_pop <- "VIU_F1"
+
+# Prepare run folder
+date <- format(Sys.time(), "%Y-%m-%d")
+input_rubias_short.FN <- gsub(pattern = "03_results/", replacement = "", x = input_rubias.FN)
+input_rubias_short.FN <- gsub(pattern = ".txt", replacement = "", x = input_rubias_short.FN)
+
+run_folder.FN <- paste0("03_results/ckmr_input_", input_rubias_short.FN, "_"
+                        , offspring_pop, "_vs_", parent_pop,"_", date
+)
+print("Making new result folder...")
+print(run_folder.FN)
+dir.create(run_folder.FN)
+
+
+# Run ckmr on the input file
+ckmr_from_rubias(input.FN = input_rubias.FN
+                 , parent_pop = parent_pop
+                 , offspring_pop = offspring_pop
+                 , cutoff = cutoff
+                 , output.dir = run_folder.FN
+)
+
+
+# Plot the output results
+graph_relatives(input.FN = "03_results/ckmr_input_rubias_142_ind_347_loc_2024-02-27_VIU_F1_vs_VIU_F0_2024-02-27/po_VIU_F0_vs_VIU_F1_pw_logl_5.txt"
+                , logl_cutoff = 5
+                , drop_string = "", directed = F
+                , plot_width = 8, plot_height = 8
+)
+
+
+
+## Pilot, first generation, CLEANED
+
+input_rubias.FN <- "03_results/rubias_142_ind_347_loc_2024-02-27.txt" # Pilot
+parent_pop <- "VIU_F0"
+offspring_pop <- "VIU_F1"
+
+# Prepare run folder
+date <- format(Sys.time(), "%Y-%m-%d")
+input_rubias_short.FN <- gsub(pattern = "03_results/", replacement = "", x = input_rubias.FN)
+input_rubias_short.FN <- gsub(pattern = ".txt", replacement = "", x = input_rubias_short.FN)
+
+run_folder.FN <- paste0("03_results/ckmr_input_", input_rubias_short.FN, "_"
+                        , offspring_pop, "_vs_", parent_pop,"_", date
+)
+print("Making new result folder...")
+print(run_folder.FN)
+dir.create(run_folder.FN)
+
+
+# Run ckmr on the input file
+ckmr_from_rubias(input.FN = input_rubias.FN
+                 , parent_pop = parent_pop
+                 , offspring_pop = offspring_pop
+                 , cutoff = cutoff
+                 , output.dir = run_folder.FN
+)
+
+
+# Plot the output results
+graph_relatives(input.FN = "03_results/ckmr_input_rubias_142_ind_347_loc_2024-02-27_VIU_F1_vs_VIU_F0_2024-02-27/po_VIU_F0_vs_VIU_F1_pw_logl_5.txt"
+                , logl_cutoff = 5
+                , drop_string = "", directed = F
+                , plot_width = 8, plot_height = 8
+)
+
+
+
+
+
+
 # Finished
